800ead2cd9ffd718afc98c536193b133e024177e
DKunin <info@dkunin.ru>
2013-09-11 15:53:04 +0400

    Package.json updates. Remove unneccesary.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,7 @@
 module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   grunt.initConfig({  
-<% if (stylusModule) { %>
-  jade: {
-    compile: {
-      options: {
-        data: {
-          debug: false
-        }
-      },
-      files: {
-        "path/to/dest.html": ["path/to/templates/*.jade", "another/path/tmpl.jade"]
-      }
-    }
-  },<%}%>  
-  <% if (stylusModule) { %>
-  stylus: {
-    compile: {
-      files: {
-      'css/nstyle.css': 'css/nstyle.styl', 
-      }
-    }
-    },
-    <%}%>  
-  watch: {
-        scripts: {
-          files: ['css/*.styl'],
-          tasks:['stylus'],
-          options: {
-            livereload: false
-          },
-        }
-    }
+    bumpup:'package.json'
   });
-
-  
   grunt.registerTask('default', ['watch']);
 };
